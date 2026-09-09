0c961ed767fc9233026f38f9c7a84a3fffe768e2
Pranav Pravakar <pranav21keshav@gmail.com>
2026-09-10 00:05:17 +0530

    Latest Changes

--- DataStructuresSwift/AppModules/DataStructures/TreeProblems.swift
+++ DataStructuresSwift/AppModules/DataStructures/TreeProblems.swift
@@ -71,6 +71,13 @@ class TreeProblems<T: Comparable & Hashable> {
      Input: root = [3,9,20,15,7]
      Output: [3.00000,14.50000,11.00000]
 
+     Input: root = [3,9,20,15,7, 11, 13]
+     Output: [3.00000,14.50000, 11.5]
+     3
+   /   \
+  9     20
+ / \    / \
+15   7  11   13
 
      Constraints:
 
